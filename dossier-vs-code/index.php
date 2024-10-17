2a08d6d76cc71fea30c41cfa7787862338e68c41
rayane riahi <rayane.noham@gmail.com>
2024-10-17 16:02:44 +0200

    J'ai crée le formulaire HTML et les requêtes SQL PHP

--- dossier-vs-code/index.php
+++ dossier-vs-code/index.php
@@ -7,5 +7,14 @@
 </head>
 <body>
     
+    <label for="sendingBox">Sending box</label>
+    <form action="request.php" method="post" name="sendingBox">
+        <input type="text" name="nickname" placeholder="nickname">
+        <input type="text" name="messageText" placeholder="message">
+        <input type="hidden" name="publicationDate">
+        <input type="hidden" name="ip" value="<?php echo $_SERVER['REMOTE_ADDR'];?>">
+        <button type="submit">Send</button>
+    </form>
+
 </body>
 </html>
