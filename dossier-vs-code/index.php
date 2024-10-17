396bb40ee4cbcba3c912ef7e607bcbdd50eafe21
Quentin Lavenir <quentin.lavenir@outlook.fr>
2024-10-17 16:52:09 +0200

    modification style

--- dossier-vs-code/index.php
+++ dossier-vs-code/index.php
@@ -4,8 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
+<body class="h-screen w-screen flex flex-row bg-pink-50">
+    <div class="relative border-4 border-gray-600 w-1/4 h-full flex flex-col py-10 px-10">
+        <p class ="text-2xl font-bold absolute top-0 left-0">User</p>
+        <li class="text-blue-600">Nicolas</li>        
+        <li class="text-blue-600">Rayane</li>
+        <li class="text-blue-600">Quentin</li>
+    </div>
     
+    <div class="w-3/4 h-full flex flex-col">
+        <div class="relative border-4 border-gray-600 h-4/5 w-full">
+            <p class="text-blue-600 absolute bottom-0 right-0 mr-14 mb-14">17.10.2024 10:50 Quentin : My name is Quentin</p>
+        </div>
+        <div class="relative border-4 border-gray-600 h-1/5 w-full">
+            <p class="text-2xl font-bold absolute bottom-0 left-0 ml-14 mb-14">Sending box / nickname</p>
+            <p class="text-blue-600 absolute bottom-0 right-0 mr-14 mb-14">Je suis en train d'écrire...</p>
+    </div>
+
 </body>
 </html>
